67bd4f2861f16b96bc2e533013dd390d9e69c6fb
Yanni Chau <ytc19@ic.ac.uk>
2020-05-23 16:31:02 +0800

    updated_factory_nodes

--- factory_dc.cpp
+++ factory_dc.cpp
@@ -6,7 +6,7 @@
 
 //////////ALGORITHM for circuits with only resistors and current sources//////////
 
-1) Take in components and sources from netlist (current sources and voltage sources work in a totally different way tho)
+done 1) Take in components and sources from netlist (current sources and voltage sources work in a totally different way tho)
 2) Determine the number of nodes (v_num)
 
 4) Construct G Matrix
@@ -23,6 +23,7 @@ int main() {
     string input;
 
     //////////TEMPORARY STORAGE/////////
+    vector<string> nodes; //keep track of number of nodes
     vector<basic_component> components; //only R for now
     vector<source> sources; //only current sources for now
     string node1, node2, name; 
@@ -31,8 +32,6 @@ int main() {
     string output_type; //triangle, square, sine etc.
 
     /////////BOOLEAN DEFINITIONS//////////
-    bool is_component = (input[0] == 'R'); //support for C and L comes later
-    bool is_source = (input[0] == 'I'); //support for voltage sources come later
 
     /////////TAKE IN INDIVIDUAL LINES and define COMPONENTS/////////
     while (getline(cin, input)) {
@@ -41,23 +40,64 @@ int main() {
         ss >> name;
         ss >> node1;
         ss >> node2;
-        ss >> value; 
+        ss >> value;
 
+        //cerr << name << " " << node1 << " " << node2 << " " << value << endl;
+
+        bool is_component = (name[0] == 'R'); //support for C and L comes later
+        bool is_source = (name[0] == 'I'); //support for voltage sources come later
+
+        //Add components/ sources to vector
         if (is_component) {
-            components.push_back(basic_component(input[0], value, node1, node2, name));     
+            components.push_back(basic_component(input[0], value, node1, node2, name)); 
         } else if (is_source) {
             sources.push_back(source(input[0], "DC", node1, node2, name, 0 , value, 0)); //only DC for now
+        } else {
+            //exit(1);
         }
 
-    input.clear();
+        /*
+        //Add nodes to a vector (version1)
+        std::vector<string>::iterator it = find(nodes.begin(), nodes.end(), node1);
+        if (it == nodes.end()) {
+            nodes.push_back(node1);
+        }
+        it = find(nodes.begin(), nodes.end(), node2);
+        if (it == nodes.end()) {
+            nodes.push_back(node2);
+        }
+        */
+
+        //Add nodes to a vector (version2)
+        if (find(nodes.begin(), nodes.end(), node1) == nodes.end()) {
+            nodes.push_back(node1);
+        }
+        if (find(nodes.begin(), nodes.end(), node2) == nodes.end()) {
+            nodes.push_back(node2);
+        }
+        
+
+        input.clear();
     }
 
-    //Now we should have a vector of components and sources.
+    //Now we should have a vector of components, sources and nodes.
 
     //////////TEST PRINT//////////
-    for (int i=0; i<components.size(); i++) {
-
+    cerr << endl << "Print components" << endl;
+    for (int i=0; i<components.size(); i++) { //print components
+        cerr << components[i].return_type() << " " << components[i].return_nodes()[0] << " " << components[i].return_nodes()[1];
+        cerr << " " << components[i].return_value(0) << endl;
     }
+    cerr << endl << "Print sources" << endl;
+    for (int i=0; i<sources.size(); i++) { //print sources
+        cerr << sources[i].return_type() << " " << sources[i].return_nodes()[0] << " " << sources[i].return_nodes()[1];
+        cerr << " " << sources[i].return_value(0) << endl;
+    }
+    cerr << endl << "Print nodes" << endl;
+    for (int i=0; i<nodes.size(); i++) {
+        cerr << nodes[i] << " ";
+    }
+    cerr << endl;
 
 }
 
