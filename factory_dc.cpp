6386c82b1091527991695e98ad60a8dfe796acde
rv519 <rv519@imperial.ac.uk>
2020-05-27 15:37:26 +0100

    Netlist parser with new node class

--- factory_dc.cpp
+++ factory_dc.cpp
@@ -19,18 +19,26 @@ done 1) Take in components and sources from netlist (current sources and voltage
 
 */
 
-
 //Search vector for a node, and adds it to the vector if the node isn't found
-vector<string> add_node(string node, vector<string> array){
-    bool not_found = true;
+vector<node*> add_nodes_to_vector(node* input_node1, node* input_node2, vector<node*> array){
+    bool not_found1 = true;
+    bool not_found2 = true;
     for(int i=0; i<array.size(); i++){
-        if (array[i] == node){
-            not_found = false;
+        if (array[i]->return_ID() == input_node1->return_ID()){
+            not_found1 = false;
         }
+	if (array[i]->return_ID() == input_node2->return_ID()){
+	    not_found2 = false;
+	}
     }
-    if (not_found){
-        array.push_back(node);
+    if (not_found1){
+        array.push_back(input_node1);
     }
+    if (not_found2){
+	array.push_back(input_node2);
+    }
+    input_node1->add_node(input_node2);
+    input_node2->add_node(input_node1);
     return array;
 }
 
@@ -39,10 +47,9 @@ int main() {
     string input;
 
     //////////TEMPORARY STORAGE/////////
-    vector<string> nodes; //keep track of number of nodes
-    vector<basic_component> components; //only R for now
-    vector<source> sources; //only current sources for now
-    string node1, node2, name; 
+    vector<node*> node_vector; //keep track of nodes and their connected components
+    vector<base_class*> components; //stores all the components in the circuit
+    string node1, node2, name;
     char type;
     float value; //for RC components
     string output_type; //triangle, square, sine etc.
@@ -58,44 +65,52 @@ int main() {
         ss >> node2;
         ss >> value;
 
+	//creating conditionals for parsing in the various components from the netlist
         bool is_component = (name[0] == 'R'); //support for C and L comes later
-        bool is_source = (name[0] == 'I'); //support for voltage sources come later
+        bool is_source = (name[0] == 'I' || name[0] == 'V'); //support for current sources and voltage sources
 
-        //Add components/ sources to vector
+	//creating dynamic storage for the nodes that have been inputted
+	node *truenode1;
+	node *truenode2;
+        if(node1[0] == 'N'){
+        	truenode1 = new node(stoi(node1.substr(1)));
+        } else {
+		truenode1 = new node(stoi(node1));
+	}
+
+	if(node2[0] == 'N'){
+                truenode2 = new node(stoi(node2.substr(1)));
+        } else {
+                truenode2 = new node(stoi(node2));
+        }
+
+	//Add components/ sources to component vector
         if (is_component) {
-            components.push_back(basic_component(input[0], value, node1, node2, name)); 
+            components.push_back(new basic_component(input[0], value, truenode1, truenode2, name));
         } else if (is_source) {
-            sources.push_back(source(input[0], "DC", node1, node2, name, 0 , value, 0)); //only DC for now
+            components.push_back(new source(input[0], "DC", truenode1, truenode2, name, 0 , value, 0)); //only DC for now
         } else {
             //exit(1);
         }
-
-        //Add nodes to a vector (version3)
-        nodes = add_node(node1, nodes);
-        nodes = add_node(node2, nodes);
-
+        //Add nodes to the node vector
+        node_vector = add_nodes_to_vector(truenode1, truenode2, node_vector);
         input.clear();
     }
 
-    //Now we should have a vector of components, sources and nodes.
+    //Now we should have a vector of components and nodes.
 
     //////////TEST PRINT//////////
     cerr << endl << "Print components" << endl;
     for (int i=0; i<components.size(); i++) { //print components
-        cerr << components[i].return_type() << " " << components[i].return_nodes()[0] << " " << components[i].return_nodes()[1];
-        cerr << " " << components[i].return_value(0) << endl;
-    }
-    cerr << endl << "Print sources" << endl;
-    for (int i=0; i<sources.size(); i++) { //print sources
-        cerr << sources[i].return_type() << " " << sources[i].return_nodes()[0] << " " << sources[i].return_nodes()[1];
-        cerr << " " << sources[i].return_value(0) << endl;
+        cerr << components[i]->return_type() << " " << components[i]->return_nodes()[0]->return_ID() << " " << components[i]->return_nodes()[1]->return_ID();
+        cerr << " " << components[i]->return_value(0) << endl;
     }
     cerr << endl << "Print nodes" << endl;
-    for (int i=0; i<nodes.size(); i++) { //print nodes
-        cerr << nodes[i] << " ";
+    for (int i=0; i<node_vector.size(); i++) { //print nodes
+        cerr << node_vector[i]->return_ID() << " ";
     }
     cerr << endl;
-
+/*
     /////////ARRAY STORAGE/////////
     const int h = nodes.size();
     const int w = nodes.size();
@@ -110,7 +125,7 @@ int main() {
     //////////NOW BEGIN INSERTING INTO ARRAY//////////
     //First insert diagonal entries
     for (int i=0; i<nodes.size(); i++) {
-        node_label = nodes[i];
+        node_label = return_ID(nodes[i]);
         for (int j=0; j<components.size(); j++) {
             if((components[j].return_nodes()[0] == node_label) || (components[j].return_nodes()[1] == node_label)) {
                 conductance += 1/(components[j].return_value(0));
@@ -139,5 +154,6 @@ int main() {
 
     //////////TEST PRINT ARRAY//////////
         cerr << g << endl;
+*/
 }
 
