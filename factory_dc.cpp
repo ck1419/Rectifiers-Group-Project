6711d2c4709566fa3c9164107d96d5249399ce67
rv519 <rv519@imperial.ac.uk>
2020-05-28 14:18:06 +0100

    Fixed conductance matrix with new node class

--- factory_dc.cpp
+++ factory_dc.cpp
@@ -23,12 +23,16 @@ done 1) Take in components and sources from netlist (current sources and voltage
 vector<node*> add_nodes_to_vector(node* input_node1, node* input_node2, base_class* current_component, vector<node*> array){
     bool not_found1 = true;
     bool not_found2 = true;
+    node* vector_node1 = input_node1;
+    node* vector_node2 = input_node2;
     for(int i=0; i<array.size(); i++){
         if (array[i]->return_ID() == input_node1->return_ID()){
             not_found1 = false;
-        }
+            vector_node1 = array[i];
+	}
 	if (array[i]->return_ID() == input_node2->return_ID()){
 	    not_found2 = false;
+	    vector_node2 = array[i];
 	}
     }
     if (not_found1){
@@ -37,10 +41,10 @@ vector<node*> add_nodes_to_vector(node* input_node1, node* input_node2, base_cla
     if (not_found2){
 	array.push_back(input_node2);
     }
-    input_node1->add_node(input_node2);
-    input_node2->add_node(input_node1);
-    input_node1->add_component(current_component);
-    input_node2->add_component(current_component);
+    vector_node1->add_node(vector_node2);
+    vector_node2->add_node(vector_node1);
+    vector_node1->add_component(current_component);
+    vector_node2->add_component(current_component);
     return array;
 }
 
