f592b524a90ead4a88a2750f2b92b431bc8aea1b
ck1419 <ck1419@imperial.ac.uk>
2020-05-29 16:07:51 +0700

    conductance matrix fix

--- factory_dc.cpp
+++ factory_dc.cpp
@@ -30,11 +30,11 @@ vector<node *> add_nodes_to_vector(node *input_node1, node *input_node2, base_cl
         if (array[i]->return_ID() == input_node1->return_ID()){
             not_found1 = false;
             vector_node1 = array[i];
-	}
-	if (array[i]->return_ID() == input_node2->return_ID()){
-	    not_found2 = false;
-	    vector_node2 = array[i];
-	}
+	    }
+        if (array[i]->return_ID() == input_node2->return_ID()){
+            not_found2 = false;
+            vector_node2 = array[i];
+        }
     }
     if (not_found1)
     {
@@ -83,35 +83,24 @@ int main()
         //creating dynamic storage for the nodes that have been inputted
         node *truenode1;
         node *truenode2;
-        if (node1[0] == 'N')
-        {
+        if (node1[0] == 'N'){
             truenode1 = new node(stoi(node1.substr(1)));
-        }
-        else
-        {
+        }else{
             truenode1 = new node(stoi(node1));
         }
 
-        if (node2[0] == 'N')
-        {
+        if (node2[0] == 'N'){
             truenode2 = new node(stoi(node2.substr(1)));
-        }
-        else
-        {
+        }else{
             truenode2 = new node(stoi(node2));
         }
 
         //Add components/ sources to component vector
-        if (is_component)
-        {
+        if (is_component){
             components.push_back(new basic_component(input[0], value, truenode1, truenode2, name));
-        }
-        else if (is_source)
-        {
+        }else if (is_source){
             components.push_back(new source(input[0], "DC", truenode1, truenode2, name, 0, value, 0)); //only DC for now
-        }
-        else
-        {
+        }else{
             //exit(1);
         }
         //Add nodes to the node vector
@@ -123,17 +112,13 @@ int main()
     //Now we should have a vector of components and nodes.
 
     //////////TEST PRINT//////////
-    cerr << endl
-         << "Print components" << endl;
-    for (int i = 0; i < components.size(); i++)
-    { //print components
+    cerr << endl << "Print components" << endl;
+    for (int i = 0; i < components.size(); i++){ //print components
         cerr << components[i]->return_type() << " " << components[i]->return_nodes()[0]->return_ID() << " " << components[i]->return_nodes()[1]->return_ID();
         cerr << " " << components[i]->return_value(0) << endl;
     }
-    cerr << endl
-         << "Print nodes" << endl;
-    for (int i = 0; i < node_vector.size(); i++)
-    { //print nodes
+    cerr << endl << "Print nodes" << endl;
+    for (int i = 0; i < node_vector.size(); i++){ //print nodes
         cerr << node_vector[i]->return_ID() << " ";
     }
     cerr << endl;
@@ -152,35 +137,29 @@ int main()
 
     //////////NOW BEGIN INSERTING INTO ARRAY//////////
     //Insert both diagonal and upper triangular entries
-    for (int i = 0; i < node_vector.size(); i++)
-    {
+    for (int i = 0; i < node_vector.size(); i++)4{
         node_ID_1 = node_vector[i]->return_ID()-1;
         no_of_components = node_vector[i]->return_components().size();
-	if(node_ID_1 == -1){
-	    continue;
-	}
-        for (int j = 0; j < no_of_components; j++)
-        {
-            if (node_vector[i]->return_components()[j]->return_type() == 'R')
-            {
+        if(node_ID_1 == -1){
+            continue;
+        }
+        for (int j = 0; j < no_of_components; j++){
+            if (node_vector[i]->return_components()[j]->return_type() == 'R'){
                 diag_conductance += 1 / (node_vector[i]->return_components()[j]->return_value(0));
                 other_conductance = 1 / (node_vector[i]->return_components()[j]->return_value(0));
-                if (node_vector[i]->return_components()[j]->return_nodes()[0]->return_ID() == node_ID_1)
-                {
+                if (node_vector[i]->return_components()[j]->return_nodes()[0]->return_ID()-1 == node_ID_1){
                     node_ID_2 = node_vector[i]->return_components()[j]->return_nodes()[1]->return_ID()-1;
-                }
-                else
-                {
+                }else{
                     node_ID_2 = node_vector[i]->return_components()[j]->return_nodes()[0]->return_ID()-1;
                 }
-		if(node_ID_2 == -1){
-		    continue;
-		}
-                    g(node_ID_1, node_ID_2) += -other_conductance;
-	    }
+                if(node_ID_2 == -1){
+                    continue;
+                }
+                g(node_ID_1, node_ID_2) += -other_conductance;
+            }
         }
-            g(node_ID_1, node_ID_1) = diag_conductance;
-            diag_conductance = 0;
+        g(node_ID_1, node_ID_1) = diag_conductance;
+        diag_conductance = 0;
     }
     //////////TEST PRINT ARRAY//////////
     cerr << g << endl;
