359a1aacbc8d96e0006f11f30bbf02ae7e50cf1e
ck1419 <ck1419@imperial.ac.uk>
2020-06-02 01:22:08 +0700

    2+ node supernode support for current matrix

--- current_matrix.hpp
+++ current_matrix.hpp
@@ -5,8 +5,17 @@ using namespace std;
 
 //RECURSIVE FUNCTION FOR SUPERNODES, TBD
 //RETURNS TOTAL OF VOLTAGES+CURRENT TO BE IN MAIN SUPERNODE NODE
-float supernode_voltage(base_class* current_component){
-    return 1;
+float supernode(base_class* current_component, float cumulative_value){
+    vector <base_class*> connected_components = current_component->return_nodes()[1]->return_components();
+    //Scans through  all the components connected at the negative side of the source
+    for (int i=0; i<connected_components.size(); i++){
+        //Checks for voltage sources, and makes sure that it won't loop back to old components
+        if (connected_components[i]->return_type() == 'V' && connected_components[i]->return_nodes()[0]->return_ID() == current_component->return_nodes()[1]->return_ID()){
+            //Recursively sends the total of the voltages up
+            cumulative_value = supernode(connected_components[i], cumulative_value);
+        }
+    }
+    return cumulative_value;
 }
 
 vector<float> find_current(vector<base_class*> all_components, vector<node*> all_nodes, float t){
@@ -32,17 +41,16 @@ vector<float> find_current(vector<base_class*> all_components, vector<node*> all
             if (all_components[i]->return_nodes()[0]->return_ID() != 0 && all_components[i]->return_nodes()[1]->return_ID() == 0){
                 temp_voltage_holder[ all_components[i]->return_nodes()[0]->return_ID()-1 ] = all_components[i]->return_value(t);
             //via positive side
-            }else if (all_components[i]->return_nodes()[1]->return_ID() != 0 && all_components[i]->return_nodes()[0]->return_ID() == 0){
+            } else if (all_components[i]->return_nodes()[1]->return_ID() != 0 && all_components[i]->return_nodes()[0]->return_ID() == 0){
                 temp_voltage_holder[ all_components[i]->return_nodes()[1]->return_ID()-1 ] = all_components[i]->return_value(t);
-            //Otherwise it is a supernode
-            }else{
-                //TBD USING SUPERNODE RECURSION
+            //checks for top of supernode
+            } else if (!all_components[i]->return_nodes()[0]->return_v_source_neg()){
+                temp_voltage_holder[ all_components[i]->return_nodes()[0]->return_ID()-1 ] = supernode(all_components[i], 0);
             }
-
-
+            //all other voltage sources are ignored
         }
     }
-    
+
     //Replace the old current values with voltage values if connected to a voltage value
     for (int i=0; i<all_nodes.size(); i++){
         if (temp_voltage_holder[i] != 0){
